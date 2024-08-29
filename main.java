812d444d31c8b2f74d56ebe9529a0fde964e85c9
aarongc43 <aarongc43@gmail.com>
2024-08-28 22:52:53 -0500

    Added ball bounce movement when it hits the paddles, adjusted a few values and constants.

--- main.java
+++ main.java
@@ -1,13 +1,15 @@
 import com.raylib.Jaylib.*;
 import static com.raylib.Jaylib.*;
 import static com.raylib.Jaylib.RAYWHITE;
+import java.lang.Math;
 
 public class main {
     // constants
     private static final int SCREEN_WIDTH = 800;
     private static final int SCREEN_HEIGHT = 800;
-    private static final int PADDLE_WIDTH = 15;
+    private static final int PADDLE_WIDTH = 10;
     private static final int PADDLE_HEIGHT = 100;
+    private static final int PADDLE_SPEED = 15;
     private static final int BALL_WIDTH = 10;
     private static final int BALL_HEIGHT = 10;
     private static float ballSpeedX = 55;
@@ -29,8 +31,8 @@ public class main {
         SetTargetFPS(60);
         // create objects
         // paddle(x,y,width, height);
-        leftPaddle = new Rectangle(0, SCREEN_HEIGHT / 2 - (PADDLE_WIDTH / 2), PADDLE_WIDTH, PADDLE_HEIGHT);
-        rightPaddle = new Rectangle(SCREEN_WIDTH - PADDLE_WIDTH, SCREEN_HEIGHT / 2 - (PADDLE_WIDTH / 2), PADDLE_WIDTH, PADDLE_HEIGHT);
+        leftPaddle = new Rectangle(PADDLE_WIDTH, SCREEN_HEIGHT / 2 - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT);
+        rightPaddle = new Rectangle(SCREEN_WIDTH - PADDLE_WIDTH * 2, SCREEN_HEIGHT / 2 - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT);
         // ball(x,y,width, height);
         ball = new Rectangle(SCREEN_WIDTH / 2 - BALL_WIDTH, SCREEN_HEIGHT / 2 - (BALL_HEIGHT / 2), BALL_WIDTH, BALL_HEIGHT);
     }
@@ -45,18 +47,22 @@ public class main {
     private static void update() {
         float deltaTime = GetFrameTime();
 
-        if (IsKeyDown(KEY_W)) leftPaddle.y(leftPaddle.y() - 15);
-        if (IsKeyDown(KEY_S)) leftPaddle.y(leftPaddle.y() + 15);
+        updatePaddles();
+        updateBallPosition(deltaTime);
+        checkBallCollision();
+        checkPaddleCollision();
+    }
 
-        if (IsKeyDown(KEY_UP)) rightPaddle.y(rightPaddle.y() - 15);
-        if (IsKeyDown(KEY_DOWN)) rightPaddle.y(rightPaddle.y() + 15);
+    private static void updatePaddles() {
+        if (IsKeyDown(KEY_W)) leftPaddle.y(leftPaddle.y() - PADDLE_SPEED);
+        if (IsKeyDown(KEY_S)) leftPaddle.y(leftPaddle.y() + PADDLE_SPEED);
+
+        if (IsKeyDown(KEY_UP)) rightPaddle.y(rightPaddle.y() - PADDLE_SPEED);
+        if (IsKeyDown(KEY_DOWN)) rightPaddle.y(rightPaddle.y() + PADDLE_SPEED);
 
         leftPaddle.y(Clamp(leftPaddle.y(), 0, SCREEN_HEIGHT - PADDLE_HEIGHT));
         rightPaddle.y(Clamp(rightPaddle.y(), 0, SCREEN_HEIGHT - PADDLE_HEIGHT));
 
-        updateBallPosition(deltaTime);
-        checkBallCollision();
-        checkPaddleCollision();
     }
 
     private static void updateBallPosition(float deltaTime) {
@@ -72,37 +78,47 @@ public class main {
             ballSpeedY *= -1;
         }
 
-        // reset X after passing left paddle
+        // checking horizontal boundaries
         if (ball.x() < 0) {
             System.out.println("passed left paddle, right paddle score");
             resetBall();
-        }
-        
-        // reset X after passing right paddle
-        if (ball.x() > SCREEN_WIDTH - BALL_WIDTH) {
+        } else if (ball.x() > SCREEN_WIDTH - BALL_WIDTH) {
             System.out.println("passed right paddle, left paddle score");
             resetBall();
         }
     }
 
-    private static void resetBall() {
-        ball.x(SCREEN_WIDTH / 2);
-        ball.y(SCREEN_HEIGHT / 2);
+    // check to see if the ball has hit the paddle
+    private static void checkPaddleCollision() {
+
+        if (CheckCollisionRecs(ball, rightPaddle) || CheckCollisionRecs(ball, leftPaddle)) {
+            float currentSpeed = (float)Math.sqrt(ballSpeedX * ballSpeedX + ballSpeedY * ballSpeedY);
+            currentSpeed *= 1.1f;
+
+            double bounceAngle = randomAngleRadians();
+            ballSpeedX = currentSpeed * (float)Math.cos(bounceAngle);
+            ballSpeedY = currentSpeed * (float)Math.sin(bounceAngle);
+
+            if (CheckCollisionRecs(ball, rightPaddle)) {
+                ballSpeedX = -Math.abs(ballSpeedX);
+                ball.x(rightPaddle.x() - ball.width() - 1);
+            } else {
+                ballSpeedX = Math.abs(ballSpeedX);
+                ball.x(leftPaddle.x() + leftPaddle.width() + 1);
+            }
+        }
     }
 
-    private static void checkPaddleCollision() {
-        if (CheckCollisionRecs(ball, rightPaddle)) {
-            if (ballSpeedX > 0) {
-                ballSpeedX *= -1.1f;
-                ball.x(rightPaddle.x() - ball.width());
-            }
-        }
+    private static double randomAngleRadians() {
+        return GetRandomValue(0, 90) * DEG2RAD;
+    }
 
-        if (CheckCollisionRecs(ball, leftPaddle)) {
-            if (ballSpeedX < 0) {
-                ballSpeedX *= -1.1f;
-            }
-        }
+    private static void resetBall() {
+        ball.x(SCREEN_WIDTH / 2 - BALL_WIDTH / 2);
+        ball.y(SCREEN_HEIGHT / 2 - BALL_HEIGHT / 2);
+
+        ballSpeedX = (Math.random() > 0.5 ? 1 : -1) * Math.abs(ballSpeedX);
+        ballSpeedY = (Math.random() > 0.5 ? 1 : -1) * Math.abs(ballSpeedY);
     }
 
     private static void render() {
